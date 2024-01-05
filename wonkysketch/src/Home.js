8bc2f1f88a1ac02b1d4b1aa974bc9f24333f6f0a
solveigua <solveig.aune@live.no>
2024-01-05 21:34:44 +0100

    :sparkles: Add education and Experience

--- wonkysketch/src/Home.js
+++ wonkysketch/src/Home.js
@@ -1,6 +1,8 @@
 import background from "./background.jpg";
 import { useScroll, useTransform } from "framer-motion";
 import { ArrowDownIcon } from "@heroicons/react/24/outline";
+import { AcademicCapIcon, BuildingOfficeIcon } from "@heroicons/react/24/solid";
+
 import "./Home.css";
 import Title from "./style";
 
@@ -19,38 +21,41 @@ function Home() {
           <Title style={{ x }}>Solveig Aune</Title>
         </div>
       </header>
-      <div>
-        <p className="mx-12 text-right">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a
-          neque laoreet, efficitur velit eu, faucibus felis. Donec vehicula quam
-          elit, ut euismod arcu ultricies eu. Maecenas porta tempus nisi, in
-          vehicula lectus lacinia in. Etiam neque velit, auctor vel commodo sit
-          amet, tempus quis nisi. Aenean malesuada vel nisl nec tincidunt.
-          Pellentesque nulla purus, condimentum at maximus at, vestibulum
-          condimentum ipsum. Nulla rhoncus metus tellus. Vestibulum lobortis
-          malesuada mauris quis rutrum. Morbi pretium enim turpis, eget sodales
-          sem lacinia a. Fusce faucibus eu urna eget rhoncus. Duis ante erat,
-          ultrices quis lacus malesuada, consectetur laoreet arcu. Quisque
-          pulvinar massa a elit dignissim consectetur. Praesent eu purus nec
-          elit sodales lacinia ut molestie turpis. Aliquam vitae rutrum lorem.
-          Donec vehicula, sapien non facilisis feugiat, felis nisi maximus
-          turpis, ac gravida felis tortor et risus. Praesent suscipit nibh sed
-          neque porttitor, eget sodales nisi interdum. Fusce tincidunt eleifend
-          neque. Etiam suscipit nibh eu bibendum vehicula. Vestibulum nec nisi
-          eu ante bibendum tincidunt. Fusce bibendum nisi eget vehicula
-          pellentesque. Etiam lacinia dolor eget risus lacinia luctus. Nam id
-          risus rutrum, sagittis felis et, malesuada metus. Curabitur ut rutrum
-          nunc, id tincidunt mi. Maecenas enim elit, vehicula vitae ex ut,
-          laoreet faucibus neque. Nam ultricies in magna quis blandit. Cras
-          consectetur magna eu justo egestas, sit amet maximus odio finibus.
-          Vivamus ornare nulla a magna sagittis, at iaculis quam condimentum.
-          Nunc lobortis, lectus fermentum interdum elementum, nibh justo egestas
-          nibh, vitae sodales libero ligula ut mauris. Nulla in felis rutrum est
-          sollicitudin dignissim. Duis eu nisl enim. Praesent cursus erat sed
-          cursus sollicitudin. Morbi vehicula purus non tortor viverra, at
-          imperdiet elit congue. Maecenas mollis consectetur lectus, vel
-          pellentesque ex consectetur sed. Nullam a ex massa.
-        </p>
+      <div className="flex flex-col ">
+        <p className="font-mono text-2xl py-2"> Education and Experience </p>
+        <div className="flex flex-col my-8">
+          <AcademicCapIcon className="h-12 w-12 text-gray-800 place-self-end mx-36" />
+          <p className="font-mono mx-12 text-right">
+            MSc in Computer Technology @ NTNU
+            <br />
+            ---
+          </p>
+        </div>
+        <div className="flex flex-col my-8">
+          <BuildingOfficeIcon className="h-12 w-12 text-gray-800 place-self-end mx-36" />
+          <p className="font-mono mx-12 text-right">
+            Analyst Level Consultant @ Netlight Oslo
+            <br />
+            ---
+          </p>
+        </div>
+        <div>
+          <p className="font-mono mx-12 text-right text-lg"> Internships </p>
+          <p className="font-mono mx-12 my-4 text-right">
+            {" "}
+            Software Developer (Consultant) Internship <br />
+            NoA Ignite <br />
+            2023
+          </p>
+          <p className="font-mono mx-12 my-4 text-right">
+            Software Engineer Internship <br />
+            Hyre <br />
+            2022
+          </p>
+          <p className="font-mono mx-12 my-4 text-right">
+            Digitalization Engineer Internship <br /> OKEA <br /> 2021
+          </p>
+        </div>
       </div>
 
       <Gallery className="Gallery" />
